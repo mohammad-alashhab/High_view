3aa5cf2fbb610dd0ae13f026fc58ad9841a7ac87
hamzatal <hamza.talllll@gmail.com>
2024-11-04 17:06:02 +0300

    enhance forgot password page - add reset password page

--- views/pages/reset.php
+++ views/pages/reset.php
@@ -3,13 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../views/public/css/forgot.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  <link rel="stylesheet" href="public/assets/css/forgot.css">
   <title>Reset Password</title>
-</head>
 
+</head>
 <body>
   <div class="auth-container">
     <div class="auth-header">
@@ -17,15 +14,17 @@
     </div>
     <div class="input-box">
       <label for="email">Email</label>
-      <i class="fas fa-envelope"></i>
       <input type="email" id="email" placeholder="Enter your email" required>
+      <i class="fas fa-envelope"></i>
     </div>
-    <button class="auth-button" id="resetButton">Reset Password</button>
+    <button class="auth-button">Reset Password</button>
     <div class="register-redirect"><br>
       <p>Remember your password? <a href="/login">Login here</a></p>
     </div>
   </div>
-
-  <script src="../../views/public/js/forgot.js"></script>
 </body>
+</html>
+  <script src="public/js/forgot.js"></script>
+</body>
+
 </html>
